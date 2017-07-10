2884996d5e01d81953460626b8591dd58811ab85
masiaban <ma.mitoconnect@gmail.com>
2017-07-10 08:59:35 +0430

    First commit

---
+++ mitosample/src/main/java/Launcher.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 80066 on 7/10/2017.
+ */
+@SpringBootApplication
+public class Launcher {
+    public static void main(String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
+}
